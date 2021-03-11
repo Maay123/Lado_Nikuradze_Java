c65ca2d57a6bd3e2348108f85257c1d004b82b4c
Maay123 <vladimer.nikuradze@gau.edu.ge>
2021-03-11 13:21:53 +0400

    refresh

--- Week_1/src/com/company/Exercise_6.java
+++ Week_1/src/com/company/Exercise_6.java
@@ -7,7 +7,7 @@ public class Exercise_6 {
         System.out.println("Exercise6");
         int x ,s=0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("X=");
+        System.out.print("X=");
         x=scanner.nextInt();
         while (x!=0){
             s+=x%10;
