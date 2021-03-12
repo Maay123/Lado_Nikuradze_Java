dea6c70afcb0f764bbb4198312c41a4dd2e65cad
Maay123 <vladimer.nikuradze@gau.edu.ge>
2021-03-12 15:44:39 +0400

    add

---
+++ Week_2/Week_2/src/com/company/Project_1/A.java
@@ -0,0 +1,23 @@
+package com.company.Project_1;
+
+import java.util.Scanner;
+
+//public class A {
+//    public int a,b;
+//
+//    public void m1(){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("a=> ");
+//        a=scanner.nextInt();
+//        System.out.print("b=> ");
+//        b=scanner.nextInt();
+//    }
+//
+//    public void m2(){
+//        System.out.println("a+b="+(a+b));
+//    }
+//
+//    public int m3(){
+//        return a*b;
+//    }
+//}
---
+++ Week_2/Week_2/src/com/company/Project_1/B.java
@@ -0,0 +1,33 @@
+package com.company.Project_1;
+
+//import java.util.Scanner;
+//
+//public class B {
+//    public int a,b,c;
+//
+//    public void m1(){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("a=>");
+//        a=sc.nextInt();
+//        System.out.print("b=>");
+//        b=sc.nextInt();
+//        System.out.print("c=>");
+//        c=sc.nextInt();
+//    }
+//    public void m2(){
+//        if(a>b && a>c)
+//            System.out.println("Max:"+a);
+//        else if(b>a && b>c)
+//            System.out.println("Max:"+b);
+//        else
+//            System.out.println("Max:"+c);
+//    }
+//    public void m3(){
+//        if(a<b && a<c)
+//            System.out.println("Min:"+a);
+//        else if(b<a && b<c)
+//            System.out.println("Min:"+b);
+//        else
+//            System.out.println("Min:"+c);
+//    }
+//}
